a7b4d4847056acd23a4a64be6bc1eda3fef298fe
Akeem <akeem.mighty@gmail.com>
2022-12-22 23:31:24 -0500

    Create Doubly Linked List Class + Created Add Elements Functionality

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -3,3 +3,15 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next, self.prev = None, None
+
+class doubly_linked_list:
+    def __init__(self):
+        self.head = None
+
+#Adding data elements
+    def push(self, NewVal):
+        NewNode = Node(NewVal)
+        NewNode.next = self.head
+        if self.head is not None:
+            self.head.prev = NewNode
+        self.head = NewNode
